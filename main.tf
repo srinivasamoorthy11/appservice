b2244539e0fbfa88943d0aa7a3d51b8807cd497d
srinivasamoorthy11 <srinivasa.moorthy@disys.com>
2023-10-06 14:57:39 +0000

    first

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {    
+  required_providers {    
+    azurerm = {    
+      source = "hashicorp/azurerm"    
+    }    
+  }    
+} 
+   
+provider "azurerm" {    
+  features {}    
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "srinivasa"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = var.service_plan_name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "app_service" {
+  name                = var.app_name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+}
